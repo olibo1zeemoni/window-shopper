4b01bf8ed9c636278c62e9ba758bedee8c3a272e
Zeemoni <gaboahene1@gmail.com>
2021-10-26 11:59:45 +0000

    first commit

---
+++ Window-shopper/Controller/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Window-shopper
+//
+//  Created by Olibo moni on 25/10/2021.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,28 @@
+//
+//  CurrencyTxtField.swift
+//  Window-shopper
+//
+//  Created by Olibo moni on 26/10/2021.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+    override func awakeFromNib() {
+                  super.awakeFromNib()
+                  backgroundColor = UIColor(#colorLiteral(red: 0.25/255, green: 0.25/255, blue: 0.25/255, alpha: 0.25))
+                  layer.cornerRadius = 5.0
+                  textAlignment = .center
+                  
+                  if let p = placeholder {
+                      let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+                      attributedPlaceholder = place
+                      textColor = UIColor.red
+                  }
+                  }
+}
+    
+
+          
+   
+
